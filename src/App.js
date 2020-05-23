1ab65cc8ae031ca93c952c99d9ba3829f063cd9c
olamilekan <adeniranolalekan35@gmail.com>
2020-05-23 18:01:26 +0100

    restore checkbox plugin

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import './App.css';
 
 class App extends Component{
 
-constructor(props) {
-    super(props);
-    this.handleClick=this.handleClick.bind(this)
-}
-handleClick(userRef){
-    console.log("received a userRef: ",userRef)
-    window.FB.AppEvents.logEvent('MessengerCheckboxUserConfirmation', null, {
-        'app_id': '291338738571430',
-        'page_id': '104295157968460',
-        'ref': 'just some values',
-        'user_ref':{userRef}
-    });
-}
+    constructor(props) {
+        super(props);
+        this.handleClick=this.handleClick.bind(this)
+    }
+    handleClick(userRef){
+        console.log("received a userRef: ",userRef)
+        window.FB.AppEvents.logEvent('MessengerCheckboxUserConfirmation', null, {
+            'app_id': '291338738571430',
+            'page_id': '104295157968460',
+            'ref': 'just some values',
+            'user_ref':{userRef}
+        });
+    }
 
     render()
     {
@@ -30,18 +30,17 @@ handleClick(userRef){
                 </header>
                 Click the checbox below to recieves updates on your order
                 <form>
-                <div className="fb-messenger-checkbox"
-                     origin="https://checkbox-plugin.herokuapp.com/"
-                    page_id="104295157968460"
-                    messenger_app_id="661504924397598"
-                    user_ref={userRef}
-                     allow_login="true"
-                        size=" medium"
-                        skin="dark"
-                        center_align="true" >
-                </div>
+                    <div className="fb-messenger-checkbox"
+                         origin="https://9bd524c3.ngrok.io/"
+                         page_id="104295157968460"
+                         messenger_app_id="661504924397598"
+                         user_ref={userRef} >
+                    </div>
                     <input type="button" onClick={(e)=>this.handleClick(userRef,e)} value="Confirm Opt-in"/>
                 </form>
+
+
+
             </div>
         );
     }
